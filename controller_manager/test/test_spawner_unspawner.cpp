7cabc7ba010040824a1be2b375e3c181a884d083
Nils-Christian Iseek <nilsmailiseke@gmail.com>
2023-11-03 14:05:41 +0000

    Added test for spawner argument --load-only

--- controller_manager/test/test_spawner_unspawner.cpp
+++ controller_manager/test/test_spawner_unspawner.cpp
@@ -274,3 +274,35 @@ TEST_F(TestLoadController, unload_on_kill)
 
   ASSERT_EQ(cm_->get_loaded_controllers().size(), 0ul);
 }
+
+
+// Define a test for different parameters
+TEST_F(TestLoadController, spawner_test_type_in_arg)
+{
+  // Provide controller type via -t argument
+  EXPECT_EQ(
+    call_spawner(
+    "ctrl_2 -c test_controller_manager -t " +
+    std::string(test_controller::TEST_CONTROLLER_CLASS_NAME)),
+    0);
+
+  ASSERT_EQ(cm_->get_loaded_controllers().size(), 1ul);
+  auto ctrl_2 = cm_->get_loaded_controllers()[0];
+  ASSERT_EQ(ctrl_2.info.name, "ctrl_2");
+  ASSERT_EQ(ctrl_2.info.type, test_controller::TEST_CONTROLLER_CLASS_NAME);
+  ASSERT_EQ(ctrl_2.c->get_state().id(), lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE);
+}
+
+TEST_F(TestLoadController, TestWithParameter)
+{
+
+  // Provide controller type via -t argument
+  EXPECT_EQ(call_spawner("ctrl_2 -c test_controller_manager --load-only "), 0);
+
+  ASSERT_EQ(cm_->get_loaded_controllers().size(), 1ul);
+  auto ctrl_2 = cm_->get_loaded_controllers()[0];
+  ASSERT_EQ(ctrl_2.info.name, "ctrl_2");
+  ASSERT_EQ(ctrl_2.info.type, test_controller::TEST_CONTROLLER_CLASS_NAME);
+  ASSERT_EQ(ctrl_2.c->get_state().id(), lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED);
+}
+
